5679844e69ca9209795d281cebcb2d6d2ba5f9a1
Frotrej <f.faszczowy@gmail.com>
2025-04-16 12:50:50 +0200

    Created outline of programs operation

---
+++ MouseWorking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Mouse_and_Keyboard_Simulation
+{
+	internal class MouseWorking
+	{
+		public void Start()
+		{
+
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Testing_Mouse_and_Keyboard_Simulation;
+//https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendinput
 Console.WriteLine("Hello, World!");
+Console.WriteLine("1 for Mouse");
+Console.WriteLine("2 for Keyboard");
+
+
+
+string userChoice = Console.ReadLine();
+if (userChoice == "1")
+{
+	MouseWorking mouseWorking = new MouseWorking();
+	mouseWorking.Start();
+
+}
+else if (userChoice == "2")
+{
+
+}
